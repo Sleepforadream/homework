1fc0096b296d45d31ff2d987c42f1c3c476a8c3c
Sleepforadream <maxnervannn@gmail.com>
2022-07-14 22:36:39 +0500

    secondtask

--- src/com/arrays/homework/homework12Variant2.java
+++ src/com/arrays/homework/homework12Variant2.java
@@ -1,90 +1,26 @@
-package com.arrays.homework;                                        //package where file is located
+package com.arrays.homework;                                                        //package where file is located
 
-import java.util.*;                                                 //import utilities for class
+import java.util.ArrayList;                                                         //import ArrayList
 
-public class homework12Variant1 {                                   //main class "MyProgram"
-    public static ArrayList<String> implString = startString();     //initialization string with keyboard input
+import static com.arrays.homework.homework12Variant1.*;                             //import class "homework12Variant1"
 
+public class homework12Variant2 {                                                   //main class "homework12Variant2"
 
-    public static void main(String[] args) {                        //program entry point
-        int x = indexShortString(implString);                       //initialization value x and add index the shortest string of array implString
-        int x1 = lengthShortString(implString);                     //initialization value x1 and add length of the shortest string of array implString
-        int y = indexLongString(implString);                        //initialization value y and add index the longest string of array implString
-        int y1 = lengthLongString(implString);                      //initialization value y1 and add length of the longest string of array implString
-        System.out.println(x + " " + x1);                           //sout index the shortest string and length of the shortest string
-        System.out.println(y + " " + y1);                           //sout index the longest string and length of the longest string
+    public static void main(String[] args) {                                        //program entry point
+
+        String x = stringsBiggerThanAverageLength(implString);                      //initialization value x and assign it strings bigger than average length in array
+        System.out.println(x);                                                      //sout strings bigger than average length in array
     }
 
-    static public ArrayList<String> startString() {                 //method then sum strings which keyboard input
+    static public String stringsBiggerThanAverageLength(ArrayList<String> str) {    //method then return strings bigger than average length in array
 
-        Scanner sc = new Scanner(System.in);                        //initialization scanner
+        String count = "";                                                          //initialization String count for storage strings bigger than average length
 
-        ArrayList<String> massive = new ArrayList<>();              //initialization ArrayList "massive"
-
-        while (sc.hasNextLine()) {                                  //loop then sum strings which keyboard input
-            String input = sc.nextLine();
-            if (!input.equals("")) {
-                massive.add(input);
-            }
-            if (input.equals("")) {                                 //if keyboard input "" exit from loop
-                break;
+        for (int i = 0; i < str.size(); i++) {                                      //loop checking if strings are greater than average length and adding them in "count"
+            if (str.get(i).length() > averageLength(str)) {
+                count = count + (str.get(i)) + " ";
             }
         }
-
-        return massive;                                             //return array with all string which keyboard input
-    }
-
-    static public int indexShortString(ArrayList<String> str) {     //method then return index the shortest string in array
-
-        int index = 0;                                              //initialization variable "index" for count index the shortest string in array
-
-        int min = (str.get(0)).length();                            //loop then calculate index the shortest string in array
-        for (int i = 0; i < str.size(); i++) {
-            if ((str.get(i)).length() < min) {
-                min = (str.get(i)).length();
-                index = i;
-            }
-        }
-
-        return index;                                               //return index the shortest string in array
-    }
-
-    static public int lengthShortString(ArrayList<String> str) {    //method then return length of the shortest string in array
-
-        int min = (str.get(0)).length();                            //loop then calculate length the shortest string in array
-        for (String s : str) {
-            if (s.length() < min) {
-                min = s.length();
-            }
-        }
-
-        return min;                                                 //return length of the shortest string in array
-    }
-
-    static public int indexLongString(ArrayList<String> str) {      //method then return index the longest string in array
-
-        int index = 0;                                              //initialization variable "index" for count index the longest string in array
-
-        int max = (str.get(0)).length();                            //loop then calculate index the longest string in array
-        for (int i = 0; i < str.size(); i++) {
-            if ((str.get(i)).length() > max) {
-                max = (str.get(i)).length();
-                index = i;
-            }
-        }
-
-        return index;                                               //return index the longest string in array
-    }
-
-    static public int lengthLongString(ArrayList<String> str) {     //method then return length of the longest string in array
-
-        int max = (str.get(0)).length();                            //loop then calculate length the longest string in array
-        for (String s : str) {
-            if (s.length() > max) {
-                max = s.length();
-            }
-        }
-
-        return max;                                                 //return length of the longest string in array
+        return count;                                                               //return strings bigger than average length in array
     }
 }
--- src/com/arrays/homework/homework12Variant4.java
+++ src/com/arrays/homework/homework12Variant4.java
@@ -1,27 +1,57 @@
-package com.arrays.homework;                                                        //package where file is located
+package com.arrays.homework;                                                                    //package where file is located
 
 import java.util.ArrayList;
 
-import static com.arrays.homework.homework12Variant1.averageLength;
 import static com.arrays.homework.homework12Variant1.implString;
 
-public class homework12Variant4 {                                                   //main class "homework12Variant3"
+public class homework12Variant4 {                                                               //main class "homework12Variant4"
 
-    public static void main(String[] args) {                                        //program entry point
+    public static void main(String[] args) {                                                    //program entry point
 
-        String x = stringsLessThanAverageLength(implString);                        //initialization value x and assign it strings less than average length in array
-        System.out.println(x);                                                      //sout strings less than average length in array
+        String z = stringMinDifferentSymbols(implString);                                       //initialize the value of z and assign it the string with the least repetition of characters
+        System.out.println(z);                                                                  //sout string with the least repeated characters
     }
 
-    static public String stringsLessThanAverageLength(ArrayList<String> str) {      //method then return strings less than average length in array
+    static public ArrayList<Integer> quantityOfDifferentSymbols(ArrayList<String> str) {        //method then return special index quantity of different symbols in array
 
-        String count = "";                                                          //initialization String count for storage strings less than average length
+        int count = 0;                                                                          //initialize counter for storage index of different symbols in string
 
-        for (int i = 0; i < str.size(); i++) {                                      //loop checking if strings are less than average length and adding them in "count"
-            if (str.get(i).length() < averageLength(str)) {
-                count = count + (str.get(i)) + " ";
+        ArrayList<Integer>arr = new ArrayList<>();                                              //initialize ArrayList for storage index of different symbols in string
+
+        for (int i = 0; i < str.size(); i++) {                                                  //mega loop for calculate index of different symbols in string
+            for (int j = 0; j < str.get(i).length(); j++) {
+                for (int k = 0; k < str.get(i).length(); k++) {
+                    if (!(Character.toString(str.get(i).charAt(j))).equals(Character.toString(str.get(i).charAt(k)))) {
+                        count++;
+                    }
+                }
             }
+            arr.add(count + str.get(i).length());
+            count = 0;
         }
-        return count;                                                               //return strings less than average length in array
+        return arr;                                                                             //return special index quantity of different symbols in array
     }
+
+    static public Integer indexMinDifferentSymbols(ArrayList<String> str) {                     //method then return index string with less repetition of characters
+
+        ArrayList<Integer> arr = quantityOfDifferentSymbols(implString);                        //initialize ArrayList and assign it array indexes quantity of different symbols
+
+        int min = (arr.get(0));                                                                 //initialize variable with 0 index for storage index comparisons in loop
+
+        int index = 0;                                                                          //initialize variable with 0 for storage index string less repetition of characters
+
+            for (int i = 0; i < arr.size(); i++) {                                              //loop for find index string less repetition of characters
+                if (arr.get(i) < min) {
+                    min = arr.get(i);
+                    index = i;
+                }
+            }
+        return index;                                                                           //return index string with less repetition of characters
+    }
+
+    static public String stringMinDifferentSymbols(ArrayList<String> str) {                     //method then return string with the least repetition of characters
+
+        return str.get(indexMinDifferentSymbols(str));                                          //return strings string with the least repetition of characters through index
+    }
+
 }
