2954caf0115e5fc47806276682bbdf1852ab1a21
Sleepforadream <maxnervannn@gmail.com>
2022-07-25 17:34:35 +0500

    secondtask

--- src/com/arrays/homework2/main.java
+++ src/com/arrays/homework2/main.java
@@ -3,13 +3,17 @@ package com.arrays.homework2;
 import java.util.*;
 
 public class main extends CarService {
+    public main(AudiFactory audiFactory, BMWFactory bmwFactory) {
+        super(audiFactory, bmwFactory);
+    }
+
     public static void main(String[] args) {
 
-        List<Car> allRandomModels = createAllRandomModels();
-        String ModelsInString = stringRepresentationOfAnArray(allRandomModels);
-        Integer NumberOfEquivalentObjects = getNumberOfEquivalentObjects(allRandomModels);
-        System.out.println(ModelsInString);
-        System.out.println(NumberOfEquivalentObjects);
+        //List<Car> allRandomModels = createAllRandomModels();
+        //String ModelsInString = stringRepresentationOfAnArray(allRandomModels);
+        //Integer NumberOfEquivalentObjects = getNumberOfEquivalentObjects(allRandomModels);
+        //System.out.println(ModelsInString);
+        //System.out.println(NumberOfEquivalentObjects);
     }
 }
 
---
+++ src/com/arrays/homework22/main.java
@@ -0,0 +1,10 @@
+package com.arrays.homework22;
+
+import java.util.Arrays;
+
+public class main extends ServiceDoor {
+
+    public static void main(String[] args) {
+        getArraysWithSameTypes();
+    }
+}
