a45a27f764ea153c8d2f3dab9e9090e24435723a
Sleepforadream <maxnervannn@gmail.com>
2022-07-26 15:46:57 +0500

    secondtask

--- src/com/arrays/homework22/ServiceDoor.java
+++ src/com/arrays/homework22/ServiceDoor.java
@@ -1,14 +1,31 @@
 package com.arrays.homework22;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static com.arrays.homework22.RandomArray.*;
-import static com.arrays.homework22.RandomArray.randomKdDoors;
 
 public class ServiceDoor {
-    public static void getArraysWithSameTypes() {
+    public static void printArraysWithSameTypes() {
         Door[] randomDoors = getRandomDoors();
         System.out.println(Arrays.toString(randomDoors));
-        System.out.println(randomSofiaDoors + "" + randomKdDoors);
+
+        ArrayList<Door> sofiaArray = new ArrayList<>();
+        ArrayList<Door> kdArray = new ArrayList<>();
+
+        for (int i = 0; i < randomDoors.length; i++){
+            if (randomDoors[i] instanceof Sofia){
+                sofiaArray.add(randomDoors[i]);
+            }
+            else {
+                kdArray.add(randomDoors[i]);
+            }
+        }
+
+        ArrayList<ArrayList<Door>> sameArray = new ArrayList<ArrayList<Door>>();
+        sameArray.add(sofiaArray);
+        sameArray.add(kdArray);
+
+        System.out.println(sameArray);
         }
     }
